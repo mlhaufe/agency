2daca0eca269bdefb973747ae4fedbef65ece2be
Michael Haufe <tno@thenewobjective.com>
2023-09-14 19:33:38 -0500

    initial commit (#1)

---
+++ webpack.config.mjs
@@ -0,0 +1,42 @@
+import path from 'path';
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url),
+    __dirname = path.dirname(__filename);
+
+export default {
+    devtool: 'source-map',
+    entry: {
+        main: './src/main.mts'
+    },
+    experiments: {
+        outputModule: true
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            { test: /\.([cm]?ts|tsx)$/, loader: "ts-loader" },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.mts', '.mjs', '.js', '.ts', '.json'],
+        extensionAlias: {
+            ".js": [".js", ".ts"],
+            ".cjs": [".cjs", ".cts"],
+            ".mjs": [".mjs", ".mts"]
+        }
+    },
+    output: {
+        clean: true,
+        library: {
+            type: 'module'
+        },
+        module: true,
+        filename: '[name].mjs',
+        path: path.resolve(__dirname, 'dist'),
+    }
+};
